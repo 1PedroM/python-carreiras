8ba873e51d6eab30bfab4f3e5c3e10d93cce5f2c
1PedroM <148162649+1PedroM@users.noreply.github.com>
2023-10-18 05:17:10 +0000

    finalizado.

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 from logging import debug
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, jsonify
+
 
 app = Flask(__name__)
+
+VAGAS =[
+   {
+     "id": 1,
+     "nome": "Analista de Sistemas",
+     "localidade": "Niteroi, RJ",
+     "salario": "R$ 4000"
+   },
+   {
+       "id": 2,
+       "nome": "Analista de dados",
+       "localidade": "Rio de Janeiro, RJ",
+       "salario": "R$ 5000"
+  },
+  {
+     "id": 1,
+     "nome": "Cientista de dados",
+     "localidade": "São Gonçalo, RJ",
+     "salario": "R$ 8000"
+   }
+]
 
 
 @app.route('/')
 def inicio():
-  return "<p>Olá mundo</p>"
+  return render_template("inicio.html", vagas=VAGAS)
+
+
+@app.route('/api/vagas')
+def api_vagas():
+  return jsonify(VAGAS)
 
 
 if __name__ == "__main__":
